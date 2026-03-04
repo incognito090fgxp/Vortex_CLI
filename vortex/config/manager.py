799533a738c303de23e7eaeee3f03bd70262a3d6
incognito090fgxp <taisumovkevin@gmail.com>
2026-03-04 17:23:03 +0400

    v0.3.1.10 Delete `example` directory...

--- vortex/config/manager.py
+++ vortex/config/manager.py
@@ -4,7 +4,7 @@ import os
 from ..registry import SETTINGS_PATH, ENV_PATH, HISTORY_PATH, PROJECT_ROOT, PACKAGE_DIR
 
 # Версия: Release.Beta.DEV.FIX
-VERSION = "0.3.1.9"
+VERSION = "0.3.1.10"
 
 # Настройки по умолчанию
 DEFAULT_SETTINGS = {
